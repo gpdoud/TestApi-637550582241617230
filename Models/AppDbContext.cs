176c01f2cee083d7a8c62e3a6bfa8a745988809f
Greg Doud <gpdoud@gmail.com>
2021-04-04 11:07:03 -0400

    Committed by GitUtil

---
+++ Models/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace TestApi.Models {
+
+    public class AppDbContext : DbContext {
+
+        public DbSet<User> Users { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) {}
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace TestApi.Models {
+
+    public class User {
+
+        public int Id { get; set; }
+        [StringLength(30), Required]
+        public string Username { get; set; }
+
+        public User() {
+        }
+    }
+}
